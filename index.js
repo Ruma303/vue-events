8dd1ff731d2491899301d74e8bb34ce08de4abd0
Ruma303 <matteoh8@hotmail.it>
2023-08-22 00:19:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const App = Vue.createApp({
+    data() {
+        return {
+            count: 0,
+        }
+    }, methods: {
+        increment(){
+            this.count++
+        }}});
+
+const mountedApp = App.mount('#app');
