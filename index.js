78feb41b632a5ebf97fc4836e5cfaa4edf2a98cc
Ruma303 <matteoh8@hotmail.it>
2023-08-23 21:50:15 +0200

    piccola modifica di stile

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const App = Vue.createApp({
     data() {
         return {
-            count: 0,
+            count: 0
         }
     }, methods: {
         increment(){
             this.count++
-        }}});
+        }
+    }
+});
 
 const mountedApp = App.mount('#app');
