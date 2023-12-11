bf61ff1f81f2d2267d11e22bbe033d10cf10d616
Ruma303 <matteoh8@hotmail.it>
2023-12-11 12:06:59 +0100

    eventi ok

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const App = Vue.createApp({
     data() {
         return {
             count: 0,
+            input: '',
+            x: null,
+            y: null,
+            currentColor: 'black',
         }
     }, methods: {
-        increment(){
+        increment() {
             this.count++
-        }}});
+        },
+        showParams(p1, p2) {
+            console.log(p1, p2);
+        },
+        handleClick(event, message) {
+            console.log(message);  // Hello, World!
+            console.log(event.target.tagName); // BUTTON
+          },
+        onInput(event) {
+            this.input = event.target.value;
+        },
+        updateCoordinates(e) {
+            this.x = e.clientX;
+            this.y = e.clientY;
+        },
+        changeArticleColor(color) {
+            this.currentColor = color;
+        }
+    }
+});
 
 const mountedApp = App.mount('#app');
